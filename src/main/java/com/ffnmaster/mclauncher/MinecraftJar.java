2eb17311cfd392992664f4cbb3df8e601fd67512
FFNMaster <at.hoogeveen@gmail.com>
2013-02-03 11:13:58 +0100

    Radicly Changed all Resource Files
    Changed reference from SK89Q to FFNMaster

---
+++ src/main/java/com/ffnmaster/mclauncher/MinecraftJar.java
@@ -0,0 +1,36 @@
+package com.ffnmaster.mclauncher;
+
+import com.ffnmaster.mclauncher.util.Util;
+import java.io.File;
+
+/**
+ * Represents a jar that contains the main Minecraft game.
+ *
+ * @author md_5
+ */
+public class MinecraftJar {
+
+    private final File file;
+    private String version;
+
+    public MinecraftJar(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getName() {
+        return file.getName();
+    }
+
+    public String getVersion() {
+        return (version == null) ? (version = Util.getMCVersion(file)) : version;
+    }
+
+    @Override
+    public String toString() {
+        return file.getName() + " (" + getVersion() + ")";
+    }
+}
